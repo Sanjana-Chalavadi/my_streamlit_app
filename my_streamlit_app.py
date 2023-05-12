487ff2e340efe328b624906755646d8ef57ed951
Sanjana Chalavadi <30472680+Sanjana-Chalavadi@users.noreply.github.com>
2023-05-12 15:59:22 -0400

    Update my_streamlit_app.py
    connected it to snowflake and loading the data

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -2,43 +2,39 @@
 # Import required libraries
 import streamlit
 from snowflake.snowpark.session import Session
-from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
 import pandas as pd
 
 
-st.title('❄️ How to connect Streamlit to a Snowflake database')
+st.title('Cloud Computing Form Table')
 
-# Establish Snowflake session
-@st.cache_resource
-def create_session():
-    return Session.builder.configs(st.secrets.snowflake).create()
+# Create Session object
+def create_session_object():
+   connection_parameters = {
+      "account": "HLB87172",
+      "user": "sanjanachalavadi95",
+      "password": "Snowflake999*",
+      "role": "PC_RIVERY_ROLE",
+      "warehouse": "COMPUTE_WH",
+      "database": "CLOUD_DB",
+      "schema": "PUBLIC"
+   }
+   session = Session.builder.configs(connection_parameters).create()
+   return session
 
-session = create_session()
-st.success("Connected to Snowflake!")
+#Load data in snowpark dataframes
+# Create Snowpark DataFrames that loads data from CLOUD_DB
+def load_data(session):
+    # FORM RESULTS TBLE
+    snow_df = session.table("PUBLIC.CC_FORM_RESULTS")
+ 
+    # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
+    pd_df = snow_df.to_pandas()
 
-# Load data table
-@st.cache_data
-def load_data(cc_form_results):
-    ## Read in data table
-    st.write(f"Here's some example data from `{cc_form_results}`:")
-    table = session.table(cc_form_results)
-    
-    ## Do some computation on it
-    table = table.limit(100)
-    
-    ## Collect the results. This will run the query and download the data
-    table = table.collect()
-    return table
+# Add header and a subheader
+st.header("FORM RESULTS")
+st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
 
-# Select and display data table
-table_name = "CLOUD_DB.PUBLIC.CC_FORM_RESULTS"
-
-## Display data table
-with st.expander("See Table"):
-    df = load_data(table_name)
-    st.dataframe(df)
-
-## Writing out data
-for row in df:
-    st.write(f"{row[0]} has a :{row[1]}:")
+if __name__ == "__main__":
+    session = create_session_object()
+    load_data(session)
