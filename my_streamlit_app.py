57f16453451c17b7c76cbbc843c515fde927da97
Sanjana Chalavadi <30472680+Sanjana-Chalavadi@users.noreply.github.com>
2023-05-12 19:56:44 -0400

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -9,8 +9,8 @@ st.title('Cloud Computing Form Table')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from cc_form_results")
-my_data_row = my_cur.fetchone()
+my_cur.execute("select * from cloud_db.public.cc_form_results")
+my_data_row = my_cur.fetcall()
 st.text("Form table display success")
 st.text(my_data_row)
 #make it look like a dataframe
