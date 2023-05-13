15b15323f5008733cc3b2a615b2b1fc47aa7cee2
Sanjana Chalavadi <30472680+Sanjana-Chalavadi@users.noreply.github.com>
2023-05-13 09:43:09 -0400

    Update my_streamlit_app.py
    updated code

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,6 +1,6 @@
 # Modified from Johannes Rieke's example code
 # Import required libraries
-import streamlit as st
+import streamlit
 import pandas as pd
 import snowflake.connector
 
@@ -11,7 +11,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from cloud_db.public.cc_form_results")
 my_data_row = my_cur.fetcall()
-st.text("Form table display success")
-st.text(my_data_row)
+streamlit.text("Form table display success")
+streamlit.text(my_data_row)
 #make it look like a dataframe
-st.dataframe(my_data_row)
+streamlit.dataframe(my_data_row)
